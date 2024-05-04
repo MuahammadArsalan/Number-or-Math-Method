f74f707d848dc6d58a0e0fec66a72c393099f606
MuhammadArsalan <muhammadarsalanforever@gmail.com>
2024-05-04 22:43:12 +0530

    Assignment on progress

---
+++ app.js
@@ -0,0 +1,131 @@
+let div = document.querySelector('.div');
+// Question No 1 :
+
+// 1. Write a program that takes a positive integer from user & 
+// display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number 
+
+
+// Solution :
+
+// let div = document.querySelector('.div');
+
+// let InputNumber = prompt('Enter any Positive number');
+// div.innerHTML = `
+// <h1>Input Number = ${InputNumber}</h1>
+// <h1>Round Value = ${Math.round(InputNumber)}</h1>
+// <h2>Floor Value = ${Math.floor(InputNumber)}</h2>
+// <h3>Ceil Vlaue = ${Math.ceil(InputNumber)}</h3>
+// `
+
+// Question No 2 :
+
+// 2. Write a program that takes a negative integer from user & 
+// display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+// Solution :
+
+
+// let div = document.querySelector('.div');
+
+// let InputNumber = prompt('Enter any Positive number');
+// div.innerHTML = `
+// <h1>Input Number = ${InputNumber}</h1>
+// <h1>Round Value = ${Math.round(InputNumber)}</h1>
+// <h2>Floor Value = ${Math.floor(InputNumber)}</h2>
+// <h3>Ceil Vlaue = ${Math.ceil(InputNumber)}</h3>
+// `
+
+
+// Question No 3 :
+
+
+// 3. Write a program that takes a positive floating point number 
+// from user & display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+// Solution :
+
+
+// let div = document.querySelector('.div');
+
+// let InputNumber = prompt('Enter any Positive Floating number');
+// div.innerHTML = `
+// <h1>Input Number = ${InputNumber}</h1>
+// <h1>Round Value = ${Math.round(InputNumber)}</h1>
+// <h2>Floor Value = ${Math.floor(InputNumber)}</h2>
+// <h3>Ceil Vlaue = ${Math.ceil(InputNumber)}</h3>
+// `
+
+
+
+        //     NOTE :: FLOOR COUNT PRECEEDING VALUE && CEIL COUNT EXCEEDING VALUE IN POSITIVE INTEGER
+        //                         WHILE
+        //  FLOOR COUNT EXCEEDING VALUE && CEIL COUNT PRECEEDING  VALUE IN NEGATIVE  INTEGER 
+
+
+
+// Question No 4 :
+
+
+// 4. Write a program that takes a negative floating point number 
+// from user & display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+// Solution :
+
+
+
+// let InputNumber = prompt('Enter any Negative Floating number');
+// div.innerHTML = `
+// <h1>Input Number = ${InputNumber}</h1>
+// <h1>Round Value = ${Math.round(InputNumber)}</h1>
+// <h2>Floor Value = ${Math.floor(InputNumber)}</h2>
+// <h3>Ceil Vlaue = ${Math.ceil(InputNumber)}</h3>
+// `
+
+
+
+// Question No 5 :
+
+// 5. Write a program that displays the absolute value of a number. 
+// E.g. absolute value of -4 is 4 & absolute value of 5 is 5 
+
+// Solution :
+
+
+// let InputNumber = prompt('Enter any Negative number');
+
+// div.innerHTML = `
+// <h1>Number is  ${InputNumber}</h1>
+// <h1> Absolut value
+// ${Math.abs(InputNumber)}</h1>
+// `
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
